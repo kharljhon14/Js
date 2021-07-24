b0efb27d012e82e3fbce0fba3a03427aaad30a77
kharljhon14 <enriquezkharl14@gmail.com>
2021-07-24 21:26:34 +0800

    Quote and preloader

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,21 +1,33 @@
+const quoteText = document.querySelector("span.quote");
+const quoteAuthorText = document.querySelector("span.author");
+const newQuoteBtn = document.querySelector("#new-btn");
+const loader = document.querySelector(".loader");
 let apiQuotes = [];
 
 //Show new Quote
-function newQuote() {
-   //Pick random quote from the array
-   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-   console.log(quote)
+async function newQuote() {
+  //Pick random quote from the array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  quoteText.textContent = quote.text;
+  quoteAuthorText.textContent = quote.author;
 }
 
 //Get quotes from API
 async function getQuotes() {
-   const apiUrl = "https://type.fit/api/quotes";
-   try {
-      const response = await fetch(apiUrl); //Get the quotes from the api
-      apiQuotes = await response.json(); //Convert to json
-      newQuote();
-   } catch (err) {}
+  try {
+    const apiUrl = "https://type.fit/api/quotes";
+    const res = await fetch(apiUrl);
+    apiQuotes = await res.json();
+    newQuote();
+    loader.classList.add("loaded");
+  } catch (err) {
+    console.log(err);
+  }
 }
-
 //On Load
 getQuotes();
+
+//Get new quote when clicked
+newQuoteBtn.onclick = () => {
+  newQuote();
+};
