5143d0dba6693afa2d23458738310f6fcefa11d3
Karl <enriquezkharl14@gmail.com>
2021-07-25 10:47:51 +0800

    Added Tweet function

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,33 +1,45 @@
 const quoteText = document.querySelector("span.quote");
 const quoteAuthorText = document.querySelector("span.author");
 const newQuoteBtn = document.querySelector("#new-btn");
+const tweetBtn = document.querySelector("#twitter-btn");
 const loader = document.querySelector(".loader");
 let apiQuotes = [];
 
+//Tweet qoute
+function tweetQuote() {
+   const tweet = `https://twitter.com/intent/tweet?text=${quoteText.textContent}  -${quoteAuthorText.textContent}`;
+   window.open(tweet);
+}
+
 //Show new Quote
 async function newQuote() {
-  //Pick random quote from the array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  quoteText.textContent = quote.text;
-  quoteAuthorText.textContent = quote.author;
+   //Pick random quote from the array
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+   quoteText.textContent = quote.text;
+   quote.author == null ? (quoteAuthorText.textContent = "Unknown") : (quoteAuthorText.textContent = quote.author);
 }
 
 //Get quotes from API
 async function getQuotes() {
-  try {
-    const apiUrl = "https://type.fit/api/quotes";
-    const res = await fetch(apiUrl);
-    apiQuotes = await res.json();
-    newQuote();
-    loader.classList.add("loaded");
-  } catch (err) {
-    console.log(err);
-  }
+   try {
+      const apiUrl = "https://type.fit/api/quotes";
+      const res = await fetch(apiUrl);
+      apiQuotes = await res.json();
+      newQuote();
+      loader.classList.add("loaded");
+   } catch (err) {
+      console.log(err);
+   }
 }
 //On Load
 getQuotes();
 
 //Get new quote when clicked
 newQuoteBtn.onclick = () => {
-  newQuote();
+   newQuote();
+};
+
+//Tweet quote when cliked
+tweetBtn.onclick = () => {
+   tweetQuote();
 };
